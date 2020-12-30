9418ecabf3f7d34331c80975049eb5810a7fd609
Dien Vo <dien.vo@asnet.com.vn>
2020-12-30 17:56:28 +0700

    Config firebase production

--- react-fuse/src/app/services/firebaseService/firebaseServiceConfig.js
+++ react-fuse/src/app/services/firebaseService/firebaseServiceConfig.js
@@ -1,10 +1,10 @@
 const prodConfig = {
-	apiKey           : "AIzaSyDgVbsE_hYQZj27WSz9igz5j33wMH8v_zI",
-	authDomain       : "react-fuse.firebaseapp.com",
-	databaseURL      : "https://react-fuse-default-rtdb.firebaseio.com/",
-	projectId        : "react-fuse",
-	storageBucket    : "react-fuse.appspot.com",
-	messagingSenderId: "955078169400"
+	apiKey           : "AIzaSyB0KITJSOPSfr5J20dQY6PlGmHgAenQPQk",
+	authDomain       : "react-fuse-production.firebaseapp.com",
+	databaseURL      : "https://react-fuse-production-default-rtdb.firebaseio.com/",
+	projectId        : "react-fuse-production",
+	storageBucket    : "react-fuse-production.appspot.com",
+	messagingSenderId: "894282411305"
 };
 
 const devConfig = {
