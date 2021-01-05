4c9dc551c67e595644c9ee52c39a2228f6b19741
Dien Vo <dien.vo@asnet.com.vn>
2021-01-05 17:29:08 +0700

    Refactor product page ui render

--- react-fuse/src/app/main/pages/products/Products.js
+++ react-fuse/src/app/main/pages/products/Products.js
@@ -3,19 +3,16 @@ import FusePageCarded from '@fuse/core/FusePageCarded';
 import ProductsHeader from './components/ProductsHeader';
 import ProductsTable from './components/ProductsTable';
 
-function ProductsPage(props) {
-
-	return (
-		<FusePageCarded
-			classes={{
-				content: 'flex',
-				header: 'min-h-72 h-72 sm:h-136 sm:min-h-136',
-			}}
-			header={<ProductsHeader />}
-			content={<ProductsTable />}
-			innerScroll
-		/>
-	);
-}
+const ProductsPage = () => (
+	<FusePageCarded
+		classes={{
+			content: 'flex',
+			header: 'min-h-72 h-72 sm:h-136 sm:min-h-136',
+		}}
+		header={<ProductsHeader />}
+		content={<ProductsTable />}
+		innerScroll
+	/>
+);
 
 export default ProductsPage;
