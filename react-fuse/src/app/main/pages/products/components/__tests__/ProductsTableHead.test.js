0bd43cfe2ed0f54a86efa66c3384373184b743ef
Dien Vo <dien.vo@asnet.com.vn>
2021-01-13 10:43:31 +0700

    Implement  unit test for table head and products header

--- react-fuse/src/app/main/pages/products/components/__tests__/ProductsTableHead.test.js
+++ react-fuse/src/app/main/pages/products/components/__tests__/ProductsTableHead.test.js
@@ -2,24 +2,36 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 import { Router } from 'react-router-dom';
+import * as reactRedux from 'react-redux';
 
 import history from '@history';
-import Products from '../Products';
+import ProductsTableHead from '../ProductsTableHead';
 
 const mockStore = configureStore();
 const store = mockStore({});
 
 describe('Products', () => {
+	const props = {
+		numSelected: 1,
+		order: {
+			direction: 'asc',
+			id: 'test',
+		},
+		onSelectAllClick: jest.fn(),
+		onRequestSort: jest.fn(),
+		rowCount: 2,
+		handleRemoveProducts: jest.fn(),
+	};
 	it('should render correctly', () => {
 		const tree = renderer
 			.create(
 				<Provider store={store}>
 					<Router history={history}>
-						<Products />
+						<ProductsTableHead {...props} />
 					</Router>
 				</Provider>
 			)
 			.toJSON();
-		expect(wrapper).toMatchSnapshot();
+		expect(tree).toMatchSnapshot();
 	});
 });
