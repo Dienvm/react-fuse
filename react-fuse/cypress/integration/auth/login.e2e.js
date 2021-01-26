7842813d5d91d1f9e30baf1cfbc4023cc96b0121
Dien Vo <dien.vo@asnet.com.vn>
2021-01-26 10:10:51 +0700

    Done handle flow login

--- react-fuse/cypress/integration/auth/login.e2e.js
+++ react-fuse/cypress/integration/auth/login.e2e.js
@@ -1,20 +1,33 @@
-describe('E2E test: Sign In page', () => {
+import { ERROR_MESSAGES } from '../../../src/app/constants'
+
+describe('E2E test: Login success case', () => {
   beforeEach(() => {
     cy.fixture('auth.json').as('auth')
   })
-  // it('should sign in success and navigate to dashboard page', () => {
-  //   cy.get('@auth').then((authData) => {
-  //     const { validUser } = authData
-  //     cy.login(validUser.email, validUser.password)
-  //   })
 
-  //   cy.location().should((loc) => {
-  //     expect(loc.href).to.eq('http://localhost:3000/dashboard')
-  //   })
-  //   cy.get('[data-cy=dashboard-header]').should('contain', 'Dashboard page')
-  // })
+  it('should sign in success and navigate to dashboard page', () => {
+    cy.get('@auth').then((authData) => {
+      const { validUser } = authData
+      cy.login(validUser.email, validUser.password)
+    })
 
-  it('should return an warning when user type wrong email or password', () => {
+    cy.location().should((loc) => {
+      expect(loc.href).to.eq('http://localhost:3000/dashboard')
+    })
+    cy.get('[data-cy=dashboard-header]').should('contain', 'Dashboard page')
+  })
+
+  afterEach(() => {
+    cy.get('[data-cy=cy-user-menu-dropdown]').click()
+    cy.contains('Logout').click()
+  })
+})
+
+describe('E2E test: Login with failure case', () => {
+  beforeEach(() => {
+    cy.fixture('auth.json').as('auth')
+  })
+  it('should return an error when user type wrong email', () => {
     cy.get('@auth').then((authData) => {
       const { wrongEmail } = authData
       cy.login(wrongEmail.email, wrongEmail.password)
@@ -23,8 +36,24 @@ describe('E2E test: Sign In page', () => {
     cy.contains('Email').should('have.class', 'Mui-error')
   })
 
-  afterEach(() => {
-    cy.get('[data-cy=cy-user-menu-dropdown]').click()
-    cy.contains('Logout').click()
+  it('should return an error when user type wrong password', () => {
+    cy.get('@auth').then((authData) => {
+      const { wrongPassword } = authData
+      cy.login(wrongPassword.email, wrongPassword.password)
+    })
+
+    cy.contains('Password').should('have.class', 'Mui-error')
+  })
+
+  it('should return an error when user type wrong email format', () => {
+    cy.get('@auth').then((authData) => {
+      const { wrongEmailFormat } = authData
+      cy.login(wrongEmailFormat.email, wrongEmailFormat.password)
+    })
+
+    cy.contains(ERROR_MESSAGES.wrongEmailFormat).should(
+      'have.class',
+      'Mui-error'
+    )
   })
 })
