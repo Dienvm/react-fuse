832d7425835a4d6bd207a25bed202479a2353d6b
Dien Vo <dien.vo@asnet.com.vn>
2021-02-02 10:25:17 +0700

    Change login URL for test

--- react-fuse/cypress/support/commands.js
+++ react-fuse/cypress/support/commands.js
@@ -2,7 +2,7 @@
 // import * as authActions from '../../src/app/auth/store/actions'
 
 Cypress.Commands.add('login', (email, password) => {
-  cy.visit('/login')
+  cy.visit('https://fuse-shop-management.netlify.app/login')
 
   cy.get('input[name="username"]').type(email).should('have.value', email)
   cy.get('input[name="password"]').type(password).should('have.value', password)
